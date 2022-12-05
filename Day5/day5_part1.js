2c802cc5eefa27bfb3fc57c2b3415b2fa416aba1
Andy Dowell <andrewdowell_84@hotmail.com>
2022-12-05 11:35:03 +0000

    day5 tidy

--- Day5/day5_part1.js
+++ Day5/day5_part1.js
@@ -27,6 +27,18 @@ function populateStacksMapWithCrates(line){
     }
 }
 
+function moveCrates(input) {
+  let line = input
+  line = line.replaceAll("move" ,"")
+  line = line.replaceAll(" from" ,"")
+  line = line.replaceAll(" to" ,"")
+  const newline = line.trim()
+  const numberOfCratesToBeMoved = parseInt(newline.split(" ")[0])
+  const indexOfStackToHaveCratesRemoved = parseInt(newline.split(" ")[1])
+  const indexOfStackToHaveCratesAdded = parseInt(newline.split(" ")[2])
+  move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded)
+}
+
 function move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded){
     const stackToHaveCratesRemoved = stacksMap.get(indexOfStackToHaveCratesRemoved)
     const stackToHaveCratesAdded = stacksMap.get(indexOfStackToHaveCratesAdded)
@@ -36,27 +48,7 @@ function move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOf
     }
 }
 
-async function preProcessInput(fileName) {
-    const fileStream = fs.createReadStream(fileName)
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    })
-    let row = 0 
-    for await (const line of rl) {
-      row++  
-      if (line.length > 0){
-        if(row == 1){
-            populateStacksMap(line)
-        }
-        
-        populateStacksMapWithCrates(line)  
-        
-      }
-    }
-  }
-
-  async function processCratesToBeMoved(fileName) {
+async function readAndProcessInput(fileName) {
     const fileStream = fs.createReadStream(fileName)
     const rl = readline.createInterface({
       input: fileStream,
@@ -66,31 +58,24 @@ async function preProcessInput(fileName) {
     for await (let line of rl) {
       row++  
       if (line.length > 0){
-        if(line.includes("move")){
-            line = line.replaceAll("move" ,"")
-            line = line.replaceAll(" from" ,"")
-            line = line.replaceAll(" to" ,"")
-            const newline = line.trim()
-            const numberOfCratesToBeMoved = parseInt(newline.split(" ")[0])
-            const indexOfStackToHaveCratesRemoved = parseInt(newline.split(" ")[1])
-            const indexOfStackToHaveCratesAdded = parseInt(newline.split(" ")[2])
-            move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded)
+        if(row == 1){
+            populateStacksMap(line)
         }
+       if(line.includes("move")){
+          moveCrates(line)
+        }else{
+          populateStacksMapWithCrates(line)  
+        }        
       }
     }
   }
 
-  async function populateStacks() {
-    await preProcessInput(actualFile)
-  }
-
-  async function moveCrates() {
-    await processCratesToBeMoved(actualFile) 
+  async function populateStacksAndMoveCrates() {
+    await readAndProcessInput(actualFile)
   }
 
   async function getResult() {
-   await populateStacks()
-   await  moveCrates()
+   await populateStacksAndMoveCrates()
    var output = ""
     for(i =1; i<=limit; i++){
         const stack = stacksMap.get(i)
--- Day5/day5_part2.js
+++ Day5/day5_part2.js
@@ -8,17 +8,18 @@ let limit = 0
 
 function populateStacksMap(line){
     limit = Math.floor(line.length/3)
-    for(let i = 1; i <= limit; i++) {
-        stacksMap.set(i, new Array())
+    for(let index = 1; index <= limit; index++) {
+        stacksMap.set(index, new Array())
     }
 }
 
 function populateStacksMapWithCrates(line){
     var count = 1
-    for(let i = 0; i <=line.length; i=i+4) {
-        var second = i+1
-        var third = i+2
-        const crate = line.charAt(i) + line.charAt(second) + line.charAt(third) 
+    for(let index = 0; index <=line.length; index=index+4) {
+        var first = index
+        var second = index+1
+        var third = index+2
+        const crate = line.charAt(first) + line.charAt(second) + line.charAt(third) 
         if(crate.includes("[")){
             stacksMap.get(count).push(crate)
         }
@@ -26,38 +27,30 @@ function populateStacksMapWithCrates(line){
     }
 }
 
-function move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded){
-    const stackToHaveCratesRemoved = stacksMap.get(indexOfStackToHaveCratesRemoved)
-    const stackToHaveCratesAdded = stacksMap.get(indexOfStackToHaveCratesAdded)
-    var tempCrate = []
-    for(let i =0; i <numberOfCratesToBeMoved; i++){
-        const crate =  stackToHaveCratesRemoved.shift()
-        tempCrate.push(crate)
-    }    
-    tempCrate.reverse().forEach(z => stackToHaveCratesAdded.unshift(z))
+function moveCrates(input) {
+  let line = input
+  line = line.replaceAll("move" ,"")
+  line = line.replaceAll(" from" ,"")
+  line = line.replaceAll(" to" ,"")
+  const newline = line.trim()
+  const numberOfCratesToBeMoved = parseInt(newline.split(" ")[0])
+  const indexOfStackToHaveCratesRemoved = parseInt(newline.split(" ")[1])
+  const indexOfStackToHaveCratesAdded = parseInt(newline.split(" ")[2])
+  move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded)
 }
 
-async function preProcessInput(fileName) {
-    const fileStream = fs.createReadStream(fileName)
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    })
-    let row = 0 
-    for await (const line of rl) {
-      row++  
-      if (line.length > 0){
-        if(row == 1){
-            populateStacksMap(line)
-        }
-        
-        populateStacksMapWithCrates(line)  
-        
-      }
-    }
-  }
+function move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded){
+  const stackToHaveCratesRemoved = stacksMap.get(indexOfStackToHaveCratesRemoved)
+  const stackToHaveCratesAdded = stacksMap.get(indexOfStackToHaveCratesAdded)
+  var tempCrate = []
+  for(let i =0; i <numberOfCratesToBeMoved; i++){
+      const crate =  stackToHaveCratesRemoved.shift()
+      tempCrate.push(crate)
+  }    
+  tempCrate.reverse().forEach(z => stackToHaveCratesAdded.unshift(z))
+}
 
-  async function processCratesToBeMoved(fileName) {
+async function readAndProcessInput(fileName) {
     const fileStream = fs.createReadStream(fileName)
     const rl = readline.createInterface({
       input: fileStream,
@@ -67,31 +60,24 @@ async function preProcessInput(fileName) {
     for await (let line of rl) {
       row++  
       if (line.length > 0){
-        if(line.includes("move")){
-            line = line.replaceAll("move" ,"")
-            line = line.replaceAll(" from" ,"")
-            line = line.replaceAll(" to" ,"")
-            const newline = line.trim()
-            const numberOfCratesToBeMoved = parseInt(newline.split(" ")[0])
-            const indexOfStackToHaveCratesRemoved = parseInt(newline.split(" ")[1])
-            const indexOfStackToHaveCratesAdded = parseInt(newline.split(" ")[2])
-            move(numberOfCratesToBeMoved, indexOfStackToHaveCratesRemoved,  indexOfStackToHaveCratesAdded)
+        if(row == 1){
+            populateStacksMap(line)
         }
+       if(line.includes("move")){
+          moveCrates(line)
+        }else{
+          populateStacksMapWithCrates(line)  
+        }        
       }
     }
   }
 
-  async function populateStacks() {
-    await preProcessInput(actualFile)
-  }
-
-  async function moveCrates() {
-    await processCratesToBeMoved(actualFile) 
+  async function populateStacksAndMoveCrates() {
+    await readAndProcessInput(actualFile)
   }
 
   async function getResult() {
-   await populateStacks()
-   await  moveCrates()
+   await populateStacksAndMoveCrates()
    var output = ""
     for(i =1; i<=limit; i++){
         const stack = stacksMap.get(i)
