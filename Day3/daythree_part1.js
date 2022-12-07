a0e10ca361f652e5f9d618ad8dab1d755123d768
Andy Dowell <andrewdowell_84@hotmail.com>
2022-12-07 17:48:24 +0000

    day3 made more functional

--- Day3/daythree_part1.js
+++ Day3/daythree_part1.js
@@ -1,45 +1,29 @@
 const fs = require('fs')
-const readline = require('readline')
-const message = "SOMETHING IS WRONG!"
-const testFile = 'test_input.txt'
-const actualFile = 'input.txt'
 const priority = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-const expectedTestValue = 157
+let output = 0
 
-function getCommonCharacterPriority(fistCompartment, secondCompartment){
-    for(let i in fistCompartment) {
-        const element = fistCompartment[i]
-        const priorityValue = priority.indexOf(element) + 1
-        if(secondCompartment.includes(element)){
-            return priorityValue
-        }
-    }
-    return 0
+const commonCharacterPriority = (fistCompartment, secondCompartment) => {
+  for(let i in fistCompartment) {
+    if(secondCompartment.includes(fistCompartment[i])) {
+      return priority.indexOf(fistCompartment[i]) + 1 
+    } 
+  }
+  return 0
 }
 
-async function processLineByLine(fileName) {
-    const fileStream = fs.createReadStream(fileName)
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    })
-    let output = 0
-    for await (const line of rl) {
-      if (line.length > 0){
-          const fistCompartment = line.slice(0, line.length / 2)
-          const secondCompartment = line.slice(line.length / 2, line.length)
-          const commonElementPrioirty = getCommonCharacterPriority(fistCompartment, secondCompartment)
-          output = output + parseInt(commonElementPrioirty)
-      }
-    }
-    return output
+const parseData = (data) => {
+  const lines = data.toString().split("\n")
+  for (let i = 0; i < lines.length; ++i) {
+    const rucksack = lines[i];
+    const fistCompartment = rucksack.substring(0, rucksack.length / 2)
+    const secondCompartment = rucksack.substring(rucksack.length / 2)
+    output = output + parseInt(commonCharacterPriority(fistCompartment, secondCompartment))
   }
+}
 
-  async function getResult() {
-    const test =  await processLineByLine(testFile)
-    console.assert(test == expectedTestValue, message)
-    const result = await processLineByLine(actualFile)
-    console.log(`output is : ${result}`)
-  }
-  
-  getResult()
+const solve = (err, data) => {
+  parseData(data)
+  console.log(`output is ${output}`)
+}
+
+fs.readFile("input.txt", solve)
