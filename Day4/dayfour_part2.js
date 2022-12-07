0f976118b4845f26037b0267a43efb674ffb18c2
Andy Dowell <andrewdowell_84@hotmail.com>
2022-12-07 18:02:34 +0000

    day4 more functional

--- Day4/dayfour_part2.js
+++ Day4/dayfour_part2.js
@@ -1,49 +1,30 @@
 const fs = require('fs')
-const readline = require('readline')
-const message = "SOMETHING IS WRONG!"
-const testFile = 'test_input.txt'
-const actualFile = 'input.txt'
-const expectedTestValue = 4
+let output = 0
 
-function getArrayOfIntegers(firstIntegerOfFirstAssignment, secondIntegerOfFirstAssignment){
-    var numbers = []
-    for(let i = firstIntegerOfFirstAssignment; i <= secondIntegerOfFirstAssignment; i++) {
-        numbers.push(i)
-    }
-    return numbers
+const array = (firstIntegerOfFirstAssignment, secondIntegerOfFirstAssignment) => {
+  var numbers = []
+  for(let i = firstIntegerOfFirstAssignment; i <= secondIntegerOfFirstAssignment; i++) {
+      numbers.push(i)
+  }
+  return numbers
 }
 
-async function processLineByLine(fileName) {
-    const fileStream = fs.createReadStream(fileName)
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    })
-    let output = 0
-    for await (const line of rl) {
-      if (line.length > 0){
-          const firstIntegerOfFirstAssignment = parseInt(line.split(",")[0].split("-")[0])
-          const secondIntegerOfFirstAssignment = parseInt(line.split(",")[0].split("-")[1])
-          const fistArray = getArrayOfIntegers(firstIntegerOfFirstAssignment,secondIntegerOfFirstAssignment)
-          const firstIntegerOfSecondAssignment = parseInt(line.split(",")[1].split("-")[0])
-          const secondIntegerOfSecondAssignment = parseInt(line.split(",")[1].split("-")[1])
-          const scondarrayArray = getArrayOfIntegers(firstIntegerOfSecondAssignment,secondIntegerOfSecondAssignment)
+const checker = (arr, target) => target.some(v => arr.includes(v));
 
-          let checker = (arr, target) => target.some(v => arr.includes(v));
-
-          if(checker(fistArray, scondarrayArray) || checker(scondarrayArray, fistArray)){
-            output++
-          }
-      }
+const parseData = (data) => {
+  const lines = data.toString().split("\n")
+  for (let i = 0; i < lines.length; ++i) {
+    const firstArray = array(parseInt(lines[i].split(",")[0].split("-")[0]), parseInt(lines[i].split(",")[0].split("-")[1]))
+    const secondArray = array(parseInt(lines[i].split(",")[1].split("-")[0]), parseInt(lines[i].split(",")[1].split("-")[1]))
+    if(checker(firstArray, secondArray) || checker(secondArray, firstArray)){
+      output++
     }
-    return output
-  }
+}
+}
 
-  async function getResult() {
-    const test =  await processLineByLine(testFile)
-    console.assert(test == expectedTestValue, message)
-    const result = await processLineByLine(actualFile)
-    console.log(`result is : ${result}`)
-  }
-  
-  getResult()
+const solve = (err, data) => {
+  parseData(data)
+  console.log(`output is ${output}`)
+}
+
+fs.readFile("input.txt", solve)
